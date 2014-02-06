f22eb249ce2851681b7089f0a55e18e1078ff1f1
AftabHussain <aftab.hussain46@gmail.com>
2014-02-06 00:19:13 -0800

    Adding stats calculator

--- src/main/java/edu/uci/ics/inf225/webcrawler/stats/StatsCalculator.java
+++ src/main/java/edu/uci/ics/inf225/webcrawler/stats/StatsCalculator.java
@@ -1,9 +1,16 @@
 package edu.uci.ics.inf225.webcrawler.stats;
 
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.url.WebURL;
 
 /**
  * Suppose you have 1 {@link Page} with N tokens. In that case, the
@@ -16,10 +23,16 @@ import edu.uci.ics.crawler4j.crawler.Page;
  */
 public class StatsCalculator {
 
-	private Map<String, Integer> subdomains;
+	public Map<String, Integer> subdomains = new HashMap<String, Integer>();
+	public Map<String, Integer> words = new HashMap<String, Integer>();
+	public Map<String, Integer> two_grams = new HashMap<String, Integer>();
+	public Map<String, Integer> pageWords = new HashMap<String, Integer>();
+	public int word_count;
+	public boolean process_2G;
+	public String twoG = "";
 
 	public StatsCalculator() {
-		this.subdomains = new HashMap<String, Integer>();
+
 	}
 
 	/**
@@ -28,7 +41,12 @@ public class StatsCalculator {
 	 * @param page
 	 */
 	public void newPage(Page page) {
+		word_count = 0;
+		process_2G = false;
+
 		// TODO Aftab to implement.
+
+		processSubdomain(page);
 	}
 
 	/**
@@ -41,8 +59,28 @@ public class StatsCalculator {
 	 *            It will be 1 if it's a single term. It will be 2 if it's a
 	 *            2-gram and so on.
 	 */
-	public void newToken(String token, int ngramity) {
+	public void newToken(String token) {
 		// TODO Aftab to implement.
+		if (words.containsKey(token) == true) {
+			words.put(token, words.get(token) + 1);
+		} else {
+			words.put(token, 1);
+		}
+		word_count++;
+
+		if (process_2G == false) {
+			twoG = token;
+			process_2G = true;
+		} else {
+			twoG = twoG + " " + token;
+			if (two_grams.containsKey(twoG) == true) {
+				two_grams.put(twoG, two_grams.get(twoG) + 1);
+			} else {
+				two_grams.put(twoG, 1);
+			}
+			twoG = token;
+		}
+
 	}
 
 	/**
@@ -53,13 +91,180 @@ public class StatsCalculator {
 	 */
 	public void closePage(Page page) {
 		// TODO Aftab to implement.
+
+		pageWords.put(page.getWebURL().getURL(), word_count);
+		twoG = "";
+		process_2G = false;
 	}
 
-	private void checkSubdomain(Page page) {
+	/*
+	 * Adds unique subdomains to the Map
+	 */
+	private void processSubdomain(Page page) {
+		WebURL url = page.getWebURL();
+		String path = url.getPath();
+		if (subdomains.containsKey(url.getSubDomain()) == true) {
+			subdomains.put(url.getSubDomain(),
+					subdomains.get(url.getSubDomain()) + 1);
+		} else {
+			subdomains.put(url.getSubDomain(), 1);
+		}
+	}
+
+	/*
+	 * Returns the number of unique pages.
+	 */
+	public long getTotalNumberOfUniquePages() {
+		return this.pageWords.size();
+	}
+
+	/*
+	 * Returns a sorted list of subdomains
+	 */
+	public List<Entry<String, Integer>> getSortedSubdomains() {
+		List<Entry<String, Integer>> list = new ArrayList<>(
+				this.subdomains.entrySet());
+
+		Collections.sort(list, new Comparator<Entry<String, Integer>>() {
+
+			@Override
+			public int compare(Entry<String, Integer> o1,
+					Entry<String, Integer> o2) {
+				return o1.getKey().compareTo(o2.getKey());
+			}
+		});
+
+		return list;
+	}
+
+	/*
+	 * Returns the URL of the Longest Page, and its length in terms of number of
+	 * words.
+	 */
+	public String getLongestPage() {
+		List<Entry<String, Integer>> list = new ArrayList<>(
+				this.pageWords.entrySet());
+
+		Collections.sort(list, new Comparator<Entry<String, Integer>>() {
+
+			@Override
+			public int compare(Entry<String, Integer> o1,
+					Entry<String, Integer> o2) {
+				return o1.getValue().compareTo(o2.getValue());
+			}
+		});
+		return list.get(list.size() - 1).getKey() + ": "
+				+ list.get(list.size() - 1).getValue();
+	}
+
+	/*
+	 * Returns a list of the 500 most common words
+	 */
+	public List<Entry<String, Integer>> mostCommonWords() {
+		List<Entry<String, Integer>> list = new ArrayList<>(
+				this.words.entrySet());
+		List<Entry<String, Integer>> list_500 = new ArrayList<>();
+
+		Collections.sort(list, new Comparator<Entry<String, Integer>>() {
+
+			@Override
+			public int compare(Entry<String, Integer> o1,
+					Entry<String, Integer> o2) {
+				return o1.getValue().compareTo(o2.getValue());
+			}
+		});
+
+		for (int i = list.size() - 1; i >= Math.max(list.size() - 500, 0); i--) {
+			list_500.add(list.get(i));
+		}
+		return list_500;
+	}
+
+	/*
+	 * Returns a list of the 20 most common twoGrams
+	 */
+	public List<Entry<String, Integer>> twoGrams() {
+		List<Entry<String, Integer>> list = new ArrayList<>(
+				this.two_grams.entrySet());
+		List<Entry<String, Integer>> list_20 = new ArrayList<>();
+
+		Collections.sort(list, new Comparator<Entry<String, Integer>>() {
+
+			@Override
+			public int compare(Entry<String, Integer> o1,
+					Entry<String, Integer> o2) {
+				return o1.getValue().compareTo(o2.getValue());
+			}
+		});
+
+		for (int i = list.size() - 1; i >= Math.max(list.size() - 20, 0); i--) {
+			list_20.add(list.get(i));
+		}
+		return list_20;
 	}
 
 	protected Object getNumberOfPagesForSubdomain(String string) {
 		// TODO Auto-generated method stub
 		return 2;
+	}
+
+	/*
+	 * PrintOutPut
+	 */
+	public void printStatistics() throws Exception {
+		List<Entry<String, Integer>> Words = this.mostCommonWords();
+		List<Entry<String, Integer>> TwoGrams = this.twoGrams();
+		String longestPage_Info = this.getLongestPage();
+		FileWriter writer = new FileWriter("Final_Statistics.txt");
+
+		writer.write("Number of unique pages: ");
+		writer.write(String.valueOf(pageWords.size()));
+		writer.write("\n");
+
+		// writer.write("Unique Pages");
+		// for (Iterator iterator = this.pageWords.entrySet().iterator();
+		// iterator.hasNext();) {
+		// Entry<String,Integer> object = (Entry<String,Integer>)
+		// iterator.next();
+		// // object.g
+		// }
+		// writer.println(pageWords);}
+
+		writer.write("Number of subdomains: ");
+		writer.write(String.valueOf(subdomains.size()));
+		writer.write("\n");
+
+		writer.write("Names of subdomains and the number of pages in each subdomain\n");
+		for (Entry<String, Integer> entry : this.subdomains.entrySet()) {
+			writer.write(entry.getKey());
+			writer.write(": ");
+			writer.write(entry.getValue().toString());
+			writer.write("\n");
+		}
+
+		writer.write("\n");
+		writer.write("Longest Page Information: ");
+		writer.write(longestPage_Info);
+		writer.write("\n");
+
+		writer.write("500 Most Common Words: \n");
+		for (int i = 0; i < Words.size(); i++) {
+			writer.write(Words.get(i).getKey());
+			writer.write(": ");
+			writer.write(Words.get(i).getValue().toString());
+			writer.write("\n");
+		}
+
+		writer.write("\n");
+		writer.write("20 most common 2-grams: \n");
+		for (int i = 0; i < TwoGrams.size(); i++) {
+			writer.write(TwoGrams.get(i).getKey());
+			writer.write(": ");
+			writer.write(TwoGrams.get(i).getValue().toString());
+			writer.write("\n");
+		}
+
+		writer.close();
+
 	}
 }
--- src/test/java/edu/uci/ics/inf225/webcrawler/stats/StatsCalculatorTest.java
+++ src/test/java/edu/uci/ics/inf225/webcrawler/stats/StatsCalculatorTest.java
@@ -1,5 +1,9 @@
 package edu.uci.ics.inf225.webcrawler.stats;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,26 +17,82 @@ public class StatsCalculatorTest {
 		/*
 		 * Expected behavior, observed behavior.
 		 */
-		Page page1 = createPage();
-		Page page2 = createPage();
+		Page page1 = createPage("ics", "uci.edu", "index.html");
+		Page page2 = createPage("ics", "uci.edu", "index2.html");
+		Page page3 = createPage("www.ics", "uci.edu", "index3.html");
 
 		// This is the code under test.
 		StatsCalculator calculator = new StatsCalculator();
 
 		calculator.newPage(page1);
-		calculator.newToken("testtoken", 1);
+		calculator.newToken("token1");
+		calculator.newToken("token2");
 		calculator.closePage(page1);
 
 		calculator.newPage(page2);
-		calculator.newToken("testtoken", 1);
+		calculator.newToken("token1");
+		calculator.newToken("token2");
 		calculator.closePage(page2);
 
+		calculator.newPage(page3);
+		calculator.newToken("token3");
+		calculator.newToken("token4");
+		calculator.closePage(page3);
+
+		/*
+		 * Subdomains: ics, www.ics (2) tokens: token1, token2, token3, token4
+		 * (4) 2 grams = "token1 token2" (2) "token 3 token4" (1) [2 different
+		 * 2-grams] Number of tokens per page: p1 = 2. p2 = 2; p3=2. Nr of pages
+		 * per subdomain: s1 = 2. s2 = 1.
+		 */
+
 		// At this point, the maps will contain the accumulated values.
-		Assert.assertEquals(2, calculator.getNumberOfPagesForSubdomain("testsubdomain"));
+		Assert.assertEquals("The total number of unique pages is wrong.", 3,
+				calculator.getTotalNumberOfUniquePages());
+		Assert.assertEquals(
+				"The number of subdomains is not the expected one.", 2,
+				calculator.subdomains.size());
+		Set<String> wordSet = new HashSet<>(Arrays.asList("token1", "token2",
+				"token3", "token4"));
+		Assert.assertEquals(
+				"The stored tokens are different from the expected ones..",
+				wordSet, calculator.words.keySet());
+		Set<String> _2gramSet = new HashSet<>(Arrays.asList("token1 token2",
+				"token3 token4"));
+		Assert.assertEquals("The 2-grams are not the expected ones.",
+				_2gramSet, calculator.two_grams.keySet());
+		Assert.assertEquals(
+				"Page 1 does not contain the right number of terms", 2,
+				calculator.pageWords.get(page1.getWebURL().getURL()).intValue());
+		Assert.assertEquals(
+				"Page 2 does not contain the right number of terms", 2,
+				calculator.pageWords.get(page2.getWebURL().getURL()).intValue());
+		Assert.assertEquals(
+				"Page 3 does not contain the right number of terms", 2,
+				calculator.pageWords.get(page3.getWebURL().getURL()).intValue());
+		Assert.assertEquals("Token 1 count is wrong ", 2,
+				calculator.words.get("token1").intValue());
+		Assert.assertEquals("Token 2 count is wrong ", 2,
+				calculator.words.get("token2").intValue());
+		Assert.assertEquals("Token 3 count is wrong ", 1,
+				calculator.words.get("token3").intValue());
+		Assert.assertEquals("Token 4 count is wrong ", 1,
+				calculator.words.get("token4").intValue());
+		Assert.assertEquals("2 Gram count of /token1 token2/ is wrong ", 2,
+				calculator.two_grams.get("token1 token2").intValue());
+
+		try {
+			calculator.printStatistics();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	private Page createPage() {
-		return new Page(new WebURL());
+	private Page createPage(String subdomain, String domain, String page) {
+		String fullURL = "http://" + subdomain + "." + domain + "/" + page;
+		WebURL url = new WebURL();
+		url.setURL(fullURL);
+		return new Page(url);
 	}
-
 }
